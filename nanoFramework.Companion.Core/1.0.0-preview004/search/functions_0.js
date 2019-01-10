389a80e1444108dc28cbff3a718a88e7d6696508
sharmavishnu <vishnusmoke@gmail.com>
2019-01-09 20:19:18 -0800

    Updates to AbstractSensor

---
+++ nanoFramework.Companion.Core/1.0.0-preview004/search/functions_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['addi2cdevice',['AddI2CDevice',['../classnano_framework_1_1_companion_1_1_core_1_1_utils_1_1_i2_c_shared_bus.html#aa907231bb7ec301246c083ed9f235532',1,'nanoFramework::Companion::Core::Utils::I2CSharedBus']]],
+  ['addressasbytes',['AddressAsBytes',['../classnano_framework_1_1_companion_1_1_abstract_memory.html#a72c3dc6b9ded8d4b0d3486291d7a24c0',1,'nanoFramework::Companion::AbstractMemory']]],
+  ['apply2pointcalibration',['Apply2PointCalibration',['../classnano_framework_1_1_companion_1_1_abstract_sensor.html#a2b8e92c52ee3b26c9411abae900e3a59',1,'nanoFramework::Companion::AbstractSensor']]],
+  ['arebytearrayequal',['AreByteArrayEqual',['../classnano_framework_1_1_companion_1_1_core_1_1_utils_1_1_array_utils.html#ab2c65d6759023248bfcc254ca84835fc',1,'nanoFramework::Companion::Core::Utils::ArrayUtils']]]
+];
